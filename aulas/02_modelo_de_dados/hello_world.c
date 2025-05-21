5333e97c89ee891e48c19e23ae3b47e6dd939f44
Maicon_Rocha <maiconpablo588@gmail.com>
2025-05-21 12:59:49 -0300

    arquivos nvs

---
+++ aulas/02_modelo_de_dados/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+
+    printf("===============================================================\n");
+    printf("                       Hello World!\n");
+    printf("     Me chamo Maicon, esse é meu primeiro codigo em C XD");
+    printf("\n=============================================================");
+
+    return 0;
+}
