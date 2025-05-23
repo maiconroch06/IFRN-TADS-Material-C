3d3ff10912eddb1d857981610b20f2ab1ce45808
Maicon_Rocha <maiconpablo588@gmail.com>
2025-05-23 18:17:39 -0300

    Update q2.c

--- PROG-Atividade-01/q2.c
+++ PROG-Atividade-01/q2.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-    char nome1,nome,nome3;
-    int minutos,segundos,centesimos;
-    //classificar os mais rapidos
-    //declarações: dizer que o recorde mundial foi ultrapassado
+    char nome1[30],nome2[30],nome3[30]; //nomes dos competidores
+    int minutos,segundos,centesimos; //tempo de prova
+    int tempo_de_prova;
+
+    printf(" -> Infome seu 'NOME': ");
+    scanf("%i",&nome1);
+    printf(" -> Infome seu 'TEMPO DE CORRIDA' (xx'xx''xx'''): ");
+    scanf("%i,%i,%i",&minutos,&segundos,&centesimos);    
+
+    if (tempo_de_prova > )
+    {
+
+    }
+    
+    tempo_de_prova = printf(" %i : %i : %i",minutos,segundos,centesimos);
+
 
 
     return 0;
