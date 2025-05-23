9ba7d6cac53fa83d4cfdc0b22d90fab2e94156d6
Maicon_Rocha <maiconpablo588@gmail.com>
2025-05-23 18:17:15 -0300

    arvs
    tentativas de resolução de quetao

--- PROG-Atividade-01/test.c
+++ PROG-Atividade-01/test.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (void) {
-    int cont = 1;
-    int aux;
+int main(void) {
+/*
+    char nome1,nome2,nome3;
+    int tempo1,tempo2,tempo3;
+    int minutos1,minutos2,minutos3;
+    int segundos1,segundos2,segundos3;
+    int centesimos1,centesimos2,centesimos3;
 
-    while (aux <= 100) {
-        aux = cont * 4;
-        printf("\n %i", aux);
-        cont++;
-    }
+    tempo1 = 100 * minutos1 + 10 * segundos1 + centesimos1;
+    tempo2 = 100 * minutos2 + 10 * segundos2 + centesimos2;
+    tempo3 = 100 * minutos3 + 10 * segundos3 + centesimos3;
+*/
+    char at1[30],aux;//at2[30],at3[30];
+    char tempo1[100];//tempo2,tempo3;
+    int minutos,segundos,centesimos;
 
-	return 0;
+    printf(" -> Infome seu 'NOME': ");
+    scanf("%s",&at1);
+
+    fflush(stdin);
+
+    printf(" -> Infome seu TEMPO DE CORRIDA: ");
+    scanf("%s, %s, %s",&minutos,&segundos,&centesimos);
+
+    tempo1 =  ('minutos', 'segundos', 'centesimos');
+
+/*    fflush(stdin);
+
+    printf(" -> Infome seu 'NOME': ");
+    scanf("%s",&at2);
+
+    printf(" -> Infome seu 'TEMPO DE CORRIDA' (xx xx xx): ");
+    scanf("%i,%i,%i",&minutos,&segundos,&centesimos);
+
+    tempo2 = 100 * minutos + 10 * segundos + centesimos;
+
+//    fflush(stdin);
+
+    printf(" -> Infome seu 'NOME': ");
+    scanf("%s",&at3);
+
+    printf(" -> Infome seu 'TEMPO DE CORRIDA' (xx xx xx): ");
+    scanf("%i,%i,%i",&minutos,&segundos,&centesimos);
+
+    tempo3 = 100 * minutos + 10 * segundos + centesimos;
+
+//    fflush(stdin); */
+
+    printf("%s",tempo1);
+
+    return 0;
 }
