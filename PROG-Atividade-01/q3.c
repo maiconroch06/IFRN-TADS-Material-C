701a460326b135cad66beba2d37cf3d2aec9c84b
Maicon_Rocha <maiconpablo588@gmail.com>
2025-05-27 23:58:16 -0300

    Resolvendo questoes
    lista avaliativa!
    q5 - feita;
    q3 - em andamento.

--- PROG-Atividade-01/q3.c
+++ PROG-Atividade-01/q3.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 
 int main(void) {
-    int idade,peso,dosagem,gotas;
-    //peso em kg
-    //converter mg para gotas
+    int idade,dosagem,gotas;
+    float peso_corporio;
 
+    printf(" Informe seu idade: ");
+    scanf("%i",&idade);
 
+    printf(" Informe seu peso (Kg): ");
+    scanf("%f",&peso_corporio);
+
+    if (idade > 0 && idade < 12) {
+        if (peso_corporio >= 5 && peso_corporio <= 9) {
+            dosagem = 125;
+        } else if (peso_corporio > 9 && peso_corporio <= 16) {
+            dosagem = 250;
+        } else if (peso_corporio > 16 && peso_corporio <= 24) {
+            dosagem = 375;
+        } else if (peso_corporio > 24 && peso_corporio <= 30) {
+            dosagem = 500;
+        } else if (peso_corporio > 30) {
+            dosagem = 750;
+        } else {
+            printf(" ERRO peso negativo");
+        }
+
+    } else if (idade >= 12) {
+        if (peso_corporio >= 60) {
+            dosagem = 1000;
+        } else if (peso_corporio > 0 && peso_corporio < 60) {
+            dosagem = 875;
+        } else {
+            printf(" ERRO peso negativo");
+        }
+        
+    } else {
+        printf(" ERRO idade negativa");
+    }
+    
+    
+
+    printf(" Quantidade de gotas: %i",dosagem);
     return 0;
 }
--- PROG-Atividade-01/q5.c
+++ PROG-Atividade-01/q5.c
@@ -1,29 +1,64 @@
 #include <stdio.h>
 
-int main(void) {
-    int op1,op2,op3;
+int main () {
+    int prato,sobremesa,bebida,cal = 0;
+
+    printf("\n-------------- PRATO PRINCIPAL --------------\n");
+    printf(" 1 - Vegetariano        | 180 cal\n");
+    printf(" 2 - Peixe              | 230 cal\n");
+    printf(" 3 - Frango             | 250 cal\n");
+    printf(" 4 - Carne              | 350 cal\n");
+
+    printf("\n-> Escolha seu prato (1-4): ");
+    scanf("%d", &prato);
+
+    switch (prato) {
+        case 1: cal += 180; break;
+        case 2: cal += 230; break;
+        case 3: cal += 250; break;
+        case 4: cal += 350; break;
+        default: break;
+        }
+
+    printf("\n---------------- SOBREMESA ------------------\n");
+    printf(" 1 - Abacaxi            |  75 cal\n");
+    printf(" 2 - Sorvete Diet       | 110 cal\n");
+    printf(" 3 - Mousse Diet        | 170 cal\n");
+    printf(" 4 - Mousse Chocolate   | 200 cal\n");
+
+    printf("\n-> Escolha sua sobremesa (1-4): ");
+    scanf("%d",&sobremesa);
+
+    switch (sobremesa) {
+        case 1: cal += 75; break;
+        case 2: cal += 110; break;
+        case 3: cal += 170; break;
+        case 4: cal += 200; break;
+        default: break;
+    }
+
+    printf("\n----------------- BEBIDA --------------------\n");
+    printf(" 1 - Chá                |  30 cal\n");
+    printf(" 2 - Suco de Laranja    |  70 cal\n");
+    printf(" 3 - Suco de Melão      | 100 cal\n");
+    printf(" 4 - Refrigerante Diet  |  65 cal\n");
+
+    printf("\n-> Escolha sua bebida (1-4): ");
+    scanf("%d",&bebida);
+
+    switch (bebida) {
+        case 1: cal += 30; break;
+        case 2: cal += 70; break;
+        case 3: cal += 100; break;
+        case 4: cal += 65; break;
+        default: break;
+    }
+
+    printf("\n=====================================================\n");
+    printf("\n               Calorias total: %i\n",cal);
+    printf("=====================================================\n");
 
 
-    printf("- - - Pratos Principais - - -");
-    printf("1.");
-    printf("2.");
-    printf("3.");
-    printf("4.");
-
-
-
-    printf("Informe seu prato principal: ");
-    scanf("%i",&op1);
-
-switch (op1)
-{
-case 1:
-    /* code */
-    break;
-
-default:
-    break;
-}
 
     return 0;
 }
