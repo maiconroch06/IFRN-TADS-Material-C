bb0fe777835de9336f03025735ea4b9aa5674c37
Maicon_Rocha <maiconpablo588@gmail.com>
2025-05-29 11:08:41 -0300

    Q2
    QUASE LÁ

--- PROG-Atividade-01/q2.c
+++ PROG-Atividade-01/q2.c
@@ -1,115 +1,104 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void){
     char atletas[3][200];
     int min[3],seg[3],cent[3];
     int tempo[3];
+    
     char aux;
+    #define AUXILAR fflush(stdin); scanf("%c",&aux);
 
-    printf("Nome do atleta 1: ");
-    scanf("%[^\n]", atletas[0]);
-    fflush(stdin);
-    scanf("%c",&aux);
+    printf(" ============================================\n");
+    printf("           COMPETETICAO DE NATACAO!\n");
 
-    printf("Informe o tempo do atleta 1:\n");
-    printf(" -> Minutos: ");
-    scanf("%i", &min[0]);
-    printf(" -> Segundos: ");
-    scanf("%i", &seg[0]);
-    printf(" -> Centesimos: ");
-    scanf("%i", &cent[0]);
-    fflush(stdin);
-    scanf("%c",&aux);
+    //Entrada de dados dos atletas e de seus respectivos tempos
+    for (int i = 0; i < 3; i++) {
+        printf(" ============================================\n");
 
-    tempo[0] = min[0] * 600 + seg[0] * 100 + cent[0];
+        //De preferencia adicionar nomes com tamanhos iguais*
+        printf("  >>> %i. Competidor: ",i + 1);
+        scanf("%[^\n]",atletas[i]);
+        AUXILIAR;
+        
+        printf(" ======== Tempo do(a) Competidor(a) %i =======\n",i + 1);
+        printf("  -> Minutos: ");
+        scanf("%i",&min[i]);
+        AUX;
 
-    printf("Nome do atleta 2: ");
-    scanf("%[^\n]", atletas[1]);
-    fflush(stdin);
-    scanf("%c",&aux);
+        printf("  -> Segundos: ");
+        scanf("%i",&seg[i]);
+        AUX;
 
-    printf("Informe o tempo do atleta 2:\n");
-    printf(" -> Minutos: ");
-    scanf("%i", &min[1]);
-    printf(" -> Segundos: ");
-    scanf("%i", &seg[1]);
-    printf(" -> Centesimos: ");
-    scanf("%i", &cent[1]);
-    fflush(stdin);
-    scanf("%c",&aux);
+        printf("  -> Centesimos: ");
+        scanf("%i",&cent[i]);
+        AUX;
 
-    tempo[1] = min[1] * 600 + seg[1] * 100 + cent[1];
+        //Converte minutos e segundos em centesimos
+        tempo[i] = min[i] * 600 + seg[i] * 100 + cent[i];
 
-    printf("Nome do atleta 3: ");
-    scanf("%[^\n]", atletas[2]);
-    fflush(stdin);
-    scanf("%c",&aux);
+    }
+    
+    //Verifica se os tempos >= 0
+    //Considerei que se o tempo == 0 seria a ausencia de algum competidor
+    if (tempo[0] >= 0 && tempo[1] >= 0 && tempo[2] >= 0) {
 
-    printf("Informe o tempo do atleta 3:\n");
-    printf(" -> Minutos: ");
-    scanf("%i", &min[2]);
-    printf(" -> Segundos: ");
-    scanf("%i", &seg[2]);
-    printf(" -> Centesimos: ");
-    scanf("%i", &cent[2]);
-    fflush(stdin);
-    scanf("%c",&aux);
+        //Classifica a ordem dos atletas comparando seus tempos
+        if (tempo[0] <= tempo[1] && tempo[1] <= tempo[2]) {
+            // ordem: 0, 1, 2
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf(" ================================================\n");
 
-    tempo[2] = min[2] * 600 + seg[2] * 100 + cent[2];
+        } else if (tempo[0] <= tempo[2] && tempo[2] <= tempo[1]) {
+            // ordem: 0, 2, 1
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf(" ================================================\n");
 
-    if (tempo[0] <= tempo[1] && tempo[1] <= tempo[2]) {
-        // ordem: 0, 1, 2
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" ================================================\n");
+        } else if (tempo[1] <= tempo[0] && tempo[0] <= tempo[2]) {
+            // ordem: 1, 0, 2
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf(" ================================================\n");
 
-    } else if (tempo[0] <= tempo[2] && tempo[2] <= tempo[1]) {
-        // ordem: 0, 2, 1
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" ================================================\n");
+        } else if (tempo[1] <= tempo[2] && tempo[2] <= tempo[0]) {
+            // ordem: 1, 2, 0
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf(" ================================================\n");
 
+        } else if (tempo[2] <= tempo[0] && tempo[0] <= tempo[1]) {
+            // ordem: 2, 0, 1
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf(" ================================================\n");
 
-    } else if (tempo[1] <= tempo[0] && tempo[0] <= tempo[2]) {
-        // ordem: 1, 0, 2
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" ================================================\n");
-
-    } else if (tempo[1] <= tempo[2] && tempo[2] <= tempo[0]) {
-        // ordem: 1, 2, 0
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" ================================================\n");
-
-    } else if (tempo[2] <= tempo[0] && tempo[0] <= tempo[1]) {
-        // ordem: 2, 0, 1
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" ================================================\n");
-
+        } else {
+            // ordem: 2, 1, 0
+            printf(" ==================== Placar ====================\n");
+            printf("  1. %s | Tempo: %i' : %i'' : %i'''\n", atletas[2], min[2], seg[2], cent[2]);
+            printf("  2. %s | Tempo: %i' : %i'' : %i'''\n", atletas[1], min[1], seg[1], cent[1]);
+            printf("  3. %s | Tempo: %i' : %i'' : %i'''\n", atletas[0], min[0], seg[0], cent[0]);
+            printf(" ================================================\n"); 
+        }
+    //Se alguns dos tempos forem negativo
     } else {
-        // ordem: 2, 1, 0
-        printf(" ==================== Placar ====================\n");
-        printf(" 1. %s | Tempo: %i : %i : %i\n", atletas[2], min[2], seg[2], cent[2]);
-        printf(" 2. %s | Tempo: %i : %i : %i\n", atletas[1], min[1], seg[1], cent[1]);
-        printf(" 3. %s | Tempo: %i : %i : %i\n", atletas[0], min[0], seg[0], cent[0]);
-        printf(" ================================================\n");
+        printf(" #ERRO: tempo negativo");
     }
 
-    int recorde = 11467; //valor em centesimos
+    //Verifica se teve a quebra do recorde mundial
     int vencedor;
+    int recorde = 11467; //valor em centesimos 
 
     if (tempo[0] <= tempo[1] && tempo[0] <= tempo[2]) {
         vencedor = 0;
@@ -118,12 +107,11 @@ int main(void){
         vencedor = 1;
 
     } else {
-        vencedor = 2;
+        vencedor = 2; 
     }
 
     if (tempo[vencedor] < recorde) {
-        printf("\n ________ RECORDE MUNDIAL QUEBRADO! ________");
-    }
-
+        printf("\n =========== RECORDE MUNDIAL QUEBRADO! ==========="); }
+    
     return 0;
 }
