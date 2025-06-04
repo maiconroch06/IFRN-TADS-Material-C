7e43743f2699f74802e5a989e34d9c00b113957f
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-04 09:46:04 -0300

    Renomeação da lista

---
+++ listas_de_execicios/lista_gpt_2_for/q1.c
@@ -0,0 +1,11 @@
+//Imprima os números de 1 a 100, um por linha.
+#include <stdio.h>
+
+int main (void) {
+    for (int i = 1; i <= 100; i++) {
+        printf("%i\n",i);
+    }
+    
+
+    return 0;
+}
