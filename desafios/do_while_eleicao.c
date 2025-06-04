5d3c4352d9a2a273e137d88bf8a3c5dada0f01b0
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-04 16:39:38 -0300

    Update do_while_eleicao.c

--- desafios/do_while_eleicao.c
+++ desafios/do_while_eleicao.c
@@ -13,17 +13,17 @@ int main(void) {
 
     while (TRUE) {
     
-        do { // Não sabemos quanro alimentos o usuario irá querer
-            printf("\n-------- URNA ELETRONICA --------\n");
-            printf(" 10 - BELTRANO\n");
-            printf(" 20 - CICLANO\n");
-            printf(" 30 - FULANO\n");
-            printf(" 0 - BRANCO\n");
-            printf("------------------------------------\n");
+        do { // Não sabemos quantos votos terá
+            printf("\n -------- URNA ELETRONICA --------\n");
+            printf("  10 - BELTRANO\n");
+            printf("  20 - CICLANO\n");
+            printf("  30 - FULANO\n");
+            printf("  0 - BRANCO\n");
+            printf(" ------------------------------------\n");
             printf(" -> Selecionar candidato: ");
             scanf("%i",&voto);
 
-            if (voto > 0) {
+            if (voto >= 0) {
                 total_votos++;
             }
 
@@ -34,52 +34,55 @@ int main(void) {
                 case 0: branco += 1; break;
                 default: if(voto > 0) nulo += 1; break; }
 
+
         } while (voto >= 0);
 
         printf("\n -> Encerrar eleicao 1-Sim?: ");
         scanf("%i",&encerrar);
         
         if (encerrar == 1) {
-            printf("- - - - - RESULTADO DA ELEICAO! - - - - -");
-            printf("\n TOTAL DE VOTOS: %i",candidato_1 + candidato_2 + candidato_3 + branco + nulo);
+            printf("\n - - - - - RESULTADO DA ELEICAO! - - - - -");
+            printf("\n TOTAL DE VOTOS: %i\n",candidato_1 + candidato_2 + candidato_3 + branco + nulo);
             
             //1 2 3
             if (candidato_1 >= candidato_2 && candidato_2 >= candidato_3) {
-                printf(" 1. BELTRANO com %i votos",candidato_1);
-                printf(" 2. CICLANO com %i votos",candidato_2);
-                printf(" 3. FULANO com %i votos",candidato_3);
+                printf(" 1. BELTRANO com %i voto(s)\n",candidato_1);
+                printf(" 2. CICLANO com %i voto(s)\n",candidato_2);
+                printf(" 3. FULANO com %i voto(s)\n",candidato_3);
             }
             //1 3 2
             else if (candidato_1 >= candidato_3 && candidato_3 >= candidato_2) {
-                printf(" 1. BELTRANO com %i votos",candidato_1);
-                printf(" 2. FULANO com %i votos",candidato_3);
-                printf(" 3. CICLANO com %i votos",candidato_2);
+                printf(" 1. BELTRANO com %i voto(s)\n",candidato_1);
+                printf(" 2. FULANO com %i voto(s)\n",candidato_3);
+                printf(" 3. CICLANO com %i voto(s)\n",candidato_2);
             }
             //2 1 3
             else if (candidato_2 >= candidato_1 && candidato_1 >= candidato_3) {
-                printf(" 1. CICLANO com %i votos",candidato_2);
-                printf(" 2. BELTRANO com %i votos",candidato_1);
-                printf(" 3. FULANO com %i votos",candidato_3);
+                printf(" 1. CICLANO com %i voto(s)\n",candidato_2);
+                printf(" 2. BELTRANO com %i voto(s)\n",candidato_1);
+                printf(" 3. FULANO com %i voto(s)\n",candidato_3);
             }
             //2 3 1
             else if (candidato_2 >= candidato_3 && candidato_3 >= candidato_1) {
-                printf(" 1. CICLANO com %i votos",candidato_2);
-                printf(" 2. FULANO com %i votos",candidato_3);
-                printf(" 3. BELTRANO com %i votos",candidato_1);
+                printf(" 1. CICLANO com %i voto(s)\n",candidato_2);
+                printf(" 2. FULANO com %i voto(s)\n",candidato_3);
+                printf(" 3. BELTRANO com %i voto(s)\n",candidato_1);
             }
             //3 2 1
             else if (candidato_3 >= candidato_2 && candidato_2 >= candidato_1) {
-                printf(" 1. FULANO com %i votos",candidato_3);
-                printf(" 2. CICLANO com %i votos",candidato_2);
-                printf(" 3. BELTRANO com %i votos",candidato_1);
+                printf(" 1. FULANO com %i voto(s)\n",candidato_3);
+                printf(" 2. CICLANO com %i voto(s)\n",candidato_2);
+                printf(" 3. BELTRANO com %i voto(s)\n",candidato_1);
             }
             //3 1 2
             else if (candidato_3 >= candidato_1 && candidato_1 >= candidato_2) {
-                printf(" 1. FULANO com %i votos",candidato_3);
-                printf(" 2. BELTRANO com %i votos",candidato_1);
-                printf(" 3. CICLANO com %i votos",candidato_2);
+                printf(" 1. FULANO com %i voto(s)\n",candidato_3);
+                printf(" 2. BELTRANO com %i voto(s)\n",candidato_1);
+                printf(" 3. CICLANO com %i voto(s)\n",candidato_2);
             }
-            
+            printf("\n VOTO(S) NULO(S): %i",nulo);
+            printf("\n VOTO(S) BRANCO(S): %i",branco);
+
             break;
         }
         
