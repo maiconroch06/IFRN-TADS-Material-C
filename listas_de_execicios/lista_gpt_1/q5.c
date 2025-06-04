2cda6c260446e98afd9fa1e3fd7709789f1bbde7
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-04 12:58:57 -0300

    Create q5.c

---
+++ listas_de_execicios/lista_gpt_1/q5.c
@@ -0,0 +1,21 @@
+//Crie um vetor de 5 inteiros, leia os valores e exiba a soma total.
+#include <stdio.h>
+
+int main () {
+    int num[4];
+    int som = 0;
+
+    for (int i = 0; i <= 4; i++) {
+        printf(" Informe o numero %i: ",i + 1);
+        scanf("%i",&num[i]);
+        som += num[i];
+    }
+
+    printf("\n => RESULTADO:");
+    for (int i = 0; i < 4; i++) {
+        printf(" %i +",num[i]);
+    }
+    printf(" %i = %i",num[4],som);
+
+    return 0;
+}
