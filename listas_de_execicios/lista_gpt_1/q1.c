e70612fe2a4754cb0ef1b7e7bee67509b214339f
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-04 09:46:17 -0300

    lista 1 gpt

--- listas_de_execicios/lista_gpt_1/q1.c
+++ listas_de_execicios/lista_gpt_1/q1.c
@@ -1,11 +1,7 @@
-//Imprima os números de 1 a 100, um por linha.
 #include <stdio.h>
 
-int main (void) {
-    for (int i = 1; i <= 100; i++) {
-        printf("%i\n",i);
-    }
-    
+int main (){
+
 
     return 0;
 }
