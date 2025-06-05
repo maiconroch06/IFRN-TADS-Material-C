a1b4127322666527f39fe61367101e5644fc7174
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-05 14:44:02 -0300

    Create q2.c

---
+++ listas_de_execicios/lista_3/q2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main (void) {
+    int num, n;
+    int maior_num = 0;
+    int menor_num = 99999;
+    int cont = 0;
+    int cont_num = 0;
+
+    for (int i = 1; i == 0; i++) {
+        printf("- - - - - MEDIA ENTRE NUMEROS - - - -\n");
+        printf(" -> Numero %i: ",i);
+        scanf("%i",&num);
+        printf("[0] - para sair");
+
+        if (num > maior_num) {
+            num = maior_num;
+        } else if (num < menor_num) {
+            num = menor_num;
+        }
+        cont += num;
+        cont_num++;
+    }
+
+    int media;
+
+    if (media != 0) {
+        media = cont / cont_num;
+    } else {
+        printf(" ERRO: ");
+        return 0;
+    }    
+
+    printf("- - - - - - - - - - - - - - -\n");
+    printf(" => RESULTADO:");
+
+    printf("\n A media eh %i:",media);
+    printf("\n O maior numero eh %i:",maior_num);
+    printf("\n O menor numero eh %i:",menor_num);
+
+    printf("\n- - - - - - - - - - - - - - -\n");
+
+
+    return 0;
+}
