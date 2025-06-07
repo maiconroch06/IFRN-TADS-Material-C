1a8b0128d692c11e30c8672d157d1a7f75457b82
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-07 15:15:41 -0300

    Create q12.c

---
+++ listas_de_execicios/lista_gpt_1/q12.c
@@ -0,0 +1,25 @@
+//Leia n e calcule o fatorial de n.
+
+#include <stdio.h>
+
+int main() {
+    int n,fatorial = 1;
+
+    printf("Digite o numero: ");
+    scanf("%i", &n);
+
+    if (n >= 0) {    
+        printf("%i! = ",n);
+
+        for (int i = n; i > 0; i--) {
+            printf("%i * ",i);
+            fatorial *= i;
+        }
+        printf("0 = %i",fatorial);
+
+    } else {
+        printf(" #ERRO: numero menor que 0");
+    }
+
+    return 0;
+}
