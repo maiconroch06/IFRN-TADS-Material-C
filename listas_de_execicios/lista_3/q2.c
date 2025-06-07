af85fb53e037a16bd3ac0f5f5823cfe7a2b321c3
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-07 16:26:10 -0300

    Update q2.c

--- listas_de_execicios/lista_3/q2.c
+++ listas_de_execicios/lista_3/q2.c
@@ -2,39 +2,46 @@
 
 int main (void) {
     int num;
-    int maior_num = 0;
-    int menor_num = 99999;
-    int cont = 0;
+    int maior_num;
+    int menor_num;
+    int som_num = 0;
     int cont_num = 0;
 
-    for (int i = 1; i == 0; i++) {
+    for (int i = 0; num != 0; i++) {
         printf("- - - - - MEDIA ENTRE NUMEROS - - - -\n");
-        printf(" -> Numero %i: ",i);
+        printf(" -> Numero %i: ",i + 1);
         scanf("%i",&num);
-        printf("[0] - para sair");
-
-        if (num > maior_num) {
-            num = maior_num;
-        } else if (num < menor_num) {
-            num = menor_num;
+        printf("[0] - para sair\n");
+        
+        if (num != 0) {
+            som_num += num; //soma dos numeros
+            cont_num++; //quantidade de numeros inseridos    
         }
-        cont += num;
-        cont_num++;
+
+        //classificador de numero menor ou maior
+        if (i == 0) {
+            maior_num = menor_num = num;     
+        } else if (num > maior_num) {
+            maior_num = num;
+        } else if (num < menor_num) {
+            menor_num = num;
+        }
+        
     }
 
-    int media;
+    int med;
 
-    if (media != 0) {
-        media = cont / cont_num;
+    if (som_num != 0) {
+        med = som_num / cont_num;
     } else {
-        printf(" ERRO: ");
+        printf(" ERRO: numeros nao inseridos!");
         return 0;
     }    
 
     printf("- - - - - - - - - - - - - - -\n");
     printf(" => RESULTADO:");
 
-    printf("\n A media eh %i:",media);
+    printf("\n A media eh %i:",med);
     printf("\n O maior numero eh %i:",maior_num);
     printf("\n O menor numero eh %i:",menor_num);
 
