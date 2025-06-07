d10c9033806a3e406a2a5eefebcc71cdcd8cc95d
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-07 15:16:02 -0300

    Create q14.c

---
+++ listas_de_execicios/lista_gpt_1/q14.c
@@ -0,0 +1,18 @@
+//Desenhe um triângulo de asteriscos com base de n linhas.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite o numero de linhas: ");
+    scanf("%i", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("i");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
