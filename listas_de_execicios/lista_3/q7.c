7c421c61cf1ff9216df84f86c24576f0436b5200
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-09 17:16:47 -0300

    Update q7.c

--- listas_de_execicios/lista_3/q7.c
+++ listas_de_execicios/lista_3/q7.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 int main (void) {
-   int N;
-   float T = 0;
+    int N;
+    float T = 0;
 
-   printf(" Informe um numero: ");
-   scanf("%i", &N);
+    printf(" Informe um numero: ");
+    scanf("%i", &N);
 
-   printf(" T = ");
+    printf(" T = ");
 
-   for (int i = 1; i <= N; i++) {
-        float termo = (float) i / (N - i + 1);
-        T += termo;
+    if (N > 0) {
+    
+        for (int i = 1; i <= N; i++) {
+            float termo = (float) i / (N - i + 1);
+            T += termo;
 
-        printf("(%i/%i)", i, N - i + 1);
-        if (i < N) {
-            printf(" + ");
+            printf("(%i/%i)", i, (N - i + 1));
+            if (i < N) {
+                printf(" + ");
+            }
         }
-   }
+        printf(" = %2f ", T);
 
-   printf(" = %2f ", T);
+    } else {
+        printf("\n # ERRO: valor de N deve ser positivo!");
+    }
 
    return 0;
 }
