68ee3e181be2b6412fc729e80e95793ef2ea5790
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-13 07:53:04 -0300

    Update q9.c

--- listas_de_execicios/lista_3/q9.c
+++ listas_de_execicios/lista_3/q9.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main (void) {
-    int manoel = 150; //2cm
-    int josue = 110; //5cm
-    
+    int manoel = 150, josue = 110; //alturas cm
+
+    for (int i = 1;  ; i++, manoel += 2, josue += 5) { 
+        if (josue > manoel) {
+            printf(" Para Josue passar de Manoel, se passara %i anos!", i);
+            break;
+        }
+    }
 
    return 0;
 }
