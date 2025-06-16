b9a84ec5a6b249a415595143cfbbc41b844eb965
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-16 17:55:14 -0300

    Update vetor.c

--- desafios/vetor.c
+++ desafios/vetor.c
@@ -2,9 +2,9 @@
 
 int main (void) {
     //nota[0] nota total
-    int nota[4];
+    int nota[4] = {0};
 
-    for (int i = 1; i < 4; i++) {
+/*    for (int i = 1; i < 4; i++) {
         printf(" Informe nota %i: ",i);
         scanf("%i",&nota[i]);
 
@@ -12,6 +12,15 @@ int main (void) {
     }
     
     printf(" Nota Final: %i", nota[0]);
+*/  printf("\n Endereco do vetor: %x", nota);
+
+    for (int i = 0; i <= 20; i++) {
+        printf("\n Endereco do vetor[%i]: %x", i, &nota[i]);
+        printf("\n Valor: %i\n", nota[i]);
+    }
+    
+        
+
 
     return 0;
 }
