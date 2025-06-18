0b8789aa53b8da40ad9ee839005ead240ef24923
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-18 16:21:05 -0300

    Create vetores_conjuntos_v1.c

---
+++ desafios/vetores_conjuntos_v1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NCJ1 10
+#define NCJ2 20
+
+int main (void) {
+    int conjunto_01 [NCJ1];
+    int conjunto_02 [NCJ2];
+
+    srand(time(NULL));
+
+    // preencendo o vetor conjunto 01
+    printf("\n CONJUNTO 1: ");
+    for (int i = 0; i < NCJ1; i++) {
+        conjunto_01[i] = rand() % 21; // 0 a 20
+        printf("%i ", conjunto_01[i]);
+    }
+
+    printf("\n CONJUNTO 2: ");
+    for (int i = 0; i < NCJ2; i++) {
+        conjunto_02[i] = rand() % 21; // 0 a 20
+        printf("%i ", conjunto_02[i]);
+    }
+
+    int cont_rept = 0;
+    printf("\n NUMEROS SEMELHANTES DOS CONJUNTOS: ");
+    for (int i = 0; i < NCJ1; i++) {
+        for (int j = 0; j < NCJ2; j++) {
+            if (conjunto_01[i] == conjunto_02[j]) {
+                printf("%i ", conjunto_01[i]);
+                cont_rept += 1;
+                break;
+            }
+        }
+    }
+    printf("\n\n * repeticoes: %i", cont_rept);
+
+    return 0;
+}
