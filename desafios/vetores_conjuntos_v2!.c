88a23344ad0f0d3e54d9231094673443e96033b9
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-18 16:21:03 -0300

    Create vetores_conjuntos_v2!.c

---
+++ desafios/vetores_conjuntos_v2!.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main (void) {
+    srand(time(NULL));
+
+    int vetor [N];
+    int n_inseridos = 0;
+    int numero; // numero que vai ser inserido no vetor
+    int existe_numero = 0; // 0 - n existe 1 - existe
+
+    printf(" NUMEROS GERADOS: ");
+    do {
+        numero = rand() % 21;
+        existe_numero = 0; // flag
+        printf("%i ", numero);
+
+        for(int i = 0; i < N; i++) {
+            if (numero == vetor [i]) {
+                existe_numero = 1; // numero ja existe no vetor
+                break;
+            }
+            
+        }
+        
+        if (!existe_numero) {
+            vetor[n_inseridos] = numero;
+            n_inseridos++;
+        }
+        
+
+    } while (n_inseridos < N);
+    
+    printf(" \n VETOR: ");
+    for (int i = 0; i < N; i++) {
+        printf("%i ", vetor[i]);
+    }
+   
+    return 0;
+}
