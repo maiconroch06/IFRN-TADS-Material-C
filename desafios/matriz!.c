2bc8ba72292aecc3b3541267d6daf8a2f21bf981
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-23 17:49:51 -0300

    Create matriz!.c

---
+++ desafios/matriz!.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int n_linhas, n_colunas;
+
+    printf(" -> Informe a quantidade de linhas: ");
+    scanf("%i",&n_linhas);
+
+    printf(" -> Informe a quantidade de colunas: ");
+    scanf("%i",&n_colunas);
+
+    if (n_linhas > 0 && n_colunas > 0) {
+        int matriz [n_linhas][n_colunas];
+
+        for (int i = 0; i < n_linhas; i++) {
+            for (int j = 0; j < n_colunas; j++) {
+                //if (i == j) {
+                //  matriz[i][j] = 1;
+                //}
+                //matriz[i][j] = 0;
+                printf("%i. %i ", i, matriz[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
