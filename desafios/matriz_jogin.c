0f1707c3c592950603ded23ba9cb49c9f22cc60c
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-25 11:39:28 -0300

    Update matriz_jogin.c

--- desafios/matriz_jogin.c
+++ desafios/matriz_jogin.c
@@ -1,33 +1,46 @@
+//gerar uma matriz 10x10, dois jogadores que descobrir as posições dos numeros 1 dentro da matriz que gera aleatorio
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//gerar uma matriz 10x10, dois jogadores que descobrir as posições dos numeros 1 dentro da matriz que gera aleatorio
-#define N_LINHAS 10
-#define N_COLUNAS 10
 
 int main(void) {
-    int matriz[N_LINHAS][N_COLUNAS] = {0};
-    //int cont_tentativas[2] = {0};
-
-    //gerar em 10 posições aleatorias de 0 a 9
-    srand(time(NULL));
-    int linha_aleatoria[10] = 1 + rand() % 1;
-        //adiciona o numero na posição
-        for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 10; j++){
-                matriz[i][j] = (1 + rand() % 1);
-            }
-            
-        }
-
-    //só para exibir matriz
-    for (int i = 0; i < N_LINHAS; i++){
-        for (int j = 0; j < N_COLUNAS; j++){
-            printf("   %i  ", matriz[i][j]);
-        }
-        printf("\n\n");
-    }
     
+    //define a quantidade de jogadores
+    int quant_jogadores;
+    printf(" -> Quantidade de jogadores (> 0): ");
+    scanf("%i", &quant_jogadores);
+
+    if (quant_jogadores > 0) {
+        //definir tamanho da matriz
+        int linhas, colunas;
+        printf(" -> Defina o tamanho da matriz (i == j): ");
+        scanf("%i %i", &linhas, &colunas);
+
+        //espaços da matriz
+        int espaco_mtz = linhas * colunas;
+
+        if (linhas == colunas) {
+            //definir quantidade de bombas (representado por 1)
+            int n_bombas;
+            int limite_bombas = linhas
+            printf(" - - - Quantidade de bombas entre 0 e %i - - -\n",);
+            printf(" -> Numero de bombas: ");
+            scanf("%i", &n_bombas);
+
+            if (n_bombas > 0 && n_bombas < (float) (linhas * colunas) / 2){
+                
+                
+            } else {
+                printf(" # ERRO: a quantidade de bombas deve ser entre 0 e %i!", (float) (linhas * colunas) / 2);
+            }
+
+        } else {
+            printf(" # ERRO: as quantidades de linhas e colunas devem ser iguais e superiores a um!");
+        }
+
+    } else {
+        printf(" # ERRO: quantidade de jogadores deve ser superior a 0!");
+    }
 
     return 0;
 }
