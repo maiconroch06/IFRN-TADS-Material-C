84da26599d22e411b6c289d6d35c28d06c1f1126
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-30 17:11:53 -0300

    Update q3_v2!.c

--- listas_de_execicios/lista_4/q3_v2!.c
+++ listas_de_execicios/lista_4/q3_v2!.c
@@ -2,26 +2,26 @@
 
 #define N_MULT 3
 
-int main(void){
+int main(void) {
 
-   int fator1[N_MULT], fator2[N_MULT], produto[N_MULT];
+     int fator1[N_MULT], fator2[N_MULT], produto[N_MULT];
 
-   for (int i = 0; i < N_MULT; i++){
-        printf(" ============ MULTIPLIQUE %i ============\n", i + 1);
-        printf(" ");
-        scanf("%i",&fator1[i]); //ele dá um expaço.. como remover esse espaço?
+     for (int i = 0; i < N_MULT; i++) {
+          printf(" ============ MULTIPLIQUE %i ============\n", i + 1);
+          printf(" ");
+          scanf("%i", &fator1[i]); // ele dá um expaço.. como remover esse espaço?
 
-        printf(" x ");
+          printf(" x ");
 
-        printf(" ");
-        scanf("%i",&fator2[i]);
+          printf(" ");
+          scanf("%i", &fator2[i]);
 
-          for (int i = 0; i < N_MULT; i++){
+          for (int i = 0; i < N_MULT; i++) {
                printf(" = ");
                produto[i] = fator1[i] * fator2[i];
                printf("%i\n", produto[i]);
-   }
-   }
-
-    return 0;
+          }
+     }
+     
+     return 0;
 }
