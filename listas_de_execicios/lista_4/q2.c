260a716775b8c74369b800adad5399979e084b40
Maicon_Rocha <117792158+maiconroch06@users.noreply.github.com>
2025-06-30 08:54:02 -0300

    Create q2.c

---
+++ listas_de_execicios/lista_4/q2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define TAM 5000
+
+int main (void) {
+
+  
+float num[TAM];
+srand(time(NULL));
+for (int i = 0; i < TAM; i++) {  
+  num[i] = (float) rand() / RAND_MAX;
+}
+
+// media dos numeros
+float media, som_num = 0;
+for (int i = 0; i < TAM; i++) {
+  som_num += num[i];
+}
+media = som_num / TAM;
+// quadrado da diferença de cada termo
+float quadrado_diferenca;
+for (int i = 0; i < TAM; i++) {
+  quadrado_diferenca[i] = pow(num[i] - media, 2);
+}
+// variância
+float som_quadr_dif = 0; V;
+for (int i = 0; i < TAM; i++) {
+  som_quadr_dif += quadrado_diferenca[i];
+}
+float V = som_quadr_dif / TAM;
+
+// desvio padrao
+float Dp = sqrt(V);
+
+// exibição
+printf(" Media dos numeros: %.2f. \n", media);
+printf(" Desvio padrao: %.2f. ", Dp);
+
+return 0;
+}
+
