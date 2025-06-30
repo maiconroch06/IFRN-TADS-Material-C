19c00f3013d7413d729299eaece2042d1f6e38dc
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-30 17:12:08 -0300

    Create struct.c

---
+++ desafios/struct/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int  main (void) {
+    int n_funcionarios;
+    
+
+    printf(" Infome a quantidade de funcionarios: ");
+    scanf("%i",&n_funcionarios);
+
+    struct info_funcionarios
+    {
+        int rg[n_funcionarios]
+    };
+    
+
+    return 0;
+}
