f328e564147d27fd7a9065b134f7a17f295868d4
Maicon_Rocha <maiconpablo588@gmail.com>
2025-06-30 22:22:57 -0300

    Update q2.c

--- listas_de_execicios/lista_4/q2.c
+++ listas_de_execicios/lista_4/q2.c
@@ -5,29 +5,32 @@
 
 #define TAM 5000
 
-int main(void) {
+int main(void)
+{
 
   float num[TAM];
   srand(time(NULL));
-  for (int i = 0; i < TAM; i++) {
-    num[i] = (float)rand() / RAND_MAX;
+  for (int i = 0; i < TAM; i++)
+  {
+    num[i] = (float) rand() / RAND_MAX;
   }
 
   // media dos numeros
   float media, som_num = 0;
-  for (int i = 0; i < TAM; i++) {
-      som_num += num[i];
+  for (int i = 0; i < TAM; i++)
+  {
+    som_num += num[i];
   }
 
   media = som_num / TAM;
   // quadrado da diferença de cada termo
-  float quadrado_diferenca;
-  for (int i = 0; i < TAM; i++) {
+  float quadrado_diferenca[TAM];
+  for (int i = 0; i < TAM; i++)
+  {
     quadrado_diferenca[i] = pow(num[i] - media, 2);
   }
   // variância
   float som_quadr_dif = 0;
-  V;
   for (int i = 0; i < TAM; i++)
   {
     som_quadr_dif += quadrado_diferenca[i];
@@ -38,8 +41,20 @@ int main(void) {
   float Dp = sqrt(V);
 
   // exibição
-  printf(" Media dos numeros: %.2f. \n", media);
-  printf(" Desvio padrao: %.2f. ", Dp);
+  printf(" Media dos numeros: %.2f \n", media);
+  printf(" Desvio padrao: %.2f ", Dp);
+
+  /*/ test
+  int num5[TAM];
+  printf("\n\n Sequencia: ");
+  for (int i = 0; i < TAM; i++)
+  {
+    num5[i] = rand();
+    printf(" %i ", num5[i]);
+    
+  }
+  
+  //*/
 
   return 0;
 }
