a7d8b5c80b5e940c64163d1c0ff062e19b3aca31
Maicon_Rocha <maiconpablo588@gmail.com>
2025-07-02 23:22:52 -0300

    Update q5.c

--- listas_de_execicios/lista_gpt_1/q5.c
+++ listas_de_execicios/lista_gpt_1/q5.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 //Crie um vetor de 5 inteiros, leia os valores e exiba a soma total.
+#define N_VALORES 5
+
 int main () {
-    int num[4];
-    int som = 0;
+    int valores[N_VALORES];
+    int soma_valores = 0;
 
-    for (int i = 0; i <= 4; i++) {
-        printf(" Informe o numero %i: ",i + 1);
-        scanf("%i",&num[i]);
-        som += num[i];
+    for (int i = 0; i < N_VALORES; i++) {
+        printf(" Informe o valoresero %i: ",i + 1);
+        scanf("%i",&valores[i]);
+        soma_valores += valores[i];
     }
 
-    printf("\n => RESULTADO:");
-    for (int i = 0; i < 4; i++) {
-        printf(" %i +",num[i]);
+    printf("\n => RESULTADO: ");
+    for (int i = 0; i < N_VALORES; i++) {
+        printf("%i",valores[i]);
+
+        if (i < N_VALORES - 1)
+        {
+            printf(" + ");
+        }
     }
-    printf(" %i = %i",num[4],som);
+    
+    printf(" = %i",soma_valores);
 
     return 0;
 }
