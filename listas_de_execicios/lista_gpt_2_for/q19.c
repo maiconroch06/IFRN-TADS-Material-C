78b557ac945dfa181a7f339f79cb6856e163c9cd
Maicon_Rocha <maiconpablo588@gmail.com>
2025-07-07 17:51:17 -0300

    Create q19.c

---
+++ listas_de_execicios/lista_gpt_2_for/q19.c
@@ -0,0 +1,26 @@
+/* 19.Monte uma pirâmide numérica como a seguir para n=5:
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+*/
+
+#include <stdio.h>
+
+#define TAMANHO 5
+
+int main (void) {
+
+    for (int i = 1; i <= TAMANHO; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf(" %i ", j);
+        }
+
+    printf("\n");
+
+    }
+    
+
+    return 0;
+}
