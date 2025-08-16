3ba90d7f077daa3d978524399343817f6c92c12e
Maicon_Rocha <maiconpablo588@gmail.com>
2025-08-15 22:32:22 -0300

    removendo linhas

--- 2-listas-exercicio/lista-04/q4.c
+++ 2-listas-exercicio/lista-04/q4.c
@@ -1,6 +1,4 @@
-/*Crie um programa em linguagem C que receba a temperatura média de cada mês do ano e armazene 
-essas temperaturas em um vetor. Em seguida, o seu programa deve determinar e imprimir em tela 
-a maior e a menor temperatura do ano e em que mês estas temperaturas aconteceram.*/
+/*Crie um programa em linguagem C que receba a temperatura média de cada mês do ano e armazene essas temperaturas em um vetor. Em seguida, o seu programa deve determinar e imprimir em tela a maior e a menor temperatura do ano e em que mês estas temperaturas aconteceram.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
